f3b81d6d48ebe6c1b68cff09d1c6813f9bd465d9
joancastror <joancastro.r@gmail.com>
2025-01-31 20:35:45 -0400

    Add project files.

---
+++ InfrastructureLayer/TaskJoanContext.cs
@@ -0,0 +1,21 @@
+﻿using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLayer
+{
+    public class TaskJoanContext : DbContext
+    {
+        public TaskJoanContext(DbContextOptions options) :
+            base(options)
+        {
+
+        }
+        public DbSet<Tareas> Tarea { get; set; }
+    }
+}
